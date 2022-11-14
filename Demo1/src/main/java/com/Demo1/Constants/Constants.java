c5b7db3de35d653bbf3e8ef87b9aa7edc2e54b58
lenovo <lenovo@LAPTOP-BH9RPJ56>
2022-11-14 11:00:40 +0400

    commit

---
+++ Demo1/src/main/java/com/Demo1/Constants/Constants.java
@@ -0,0 +1,13 @@
+package com.Demo1.Constants;
+
+public class Constants {
+	public static final String CHROMEDRIVER="webdriver.chrome.driver";
+	public static final String EDGEDRIVER="webdriver.edge.driver";
+	public static final String FIREFOXDRIVER="webdriver.gecko.driver";
+	public static final String BROWSERNOTCORRECT="browser is not correct";
+	public static final String FIREFOXBROWSER="firefox";
+	public static final String CHROMEBROWSER="chrome";
+	public static final String EDGEBROWSER="edge";
+	public static final String LOGINCREDENTIALS="C:\\Users\\lenovo\\Desktop\\selenium\\workspace1\\src\\main\\resources\\excel\\Login.xlsx";
+
+}
